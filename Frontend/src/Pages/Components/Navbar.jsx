3be3be4a8d83e9bd9ebe9c743ad6283bdb07be4d
Nachiket Pensalwar <116742714+NaChIkEt-pen@users.noreply.github.com>
2024-02-02 09:04:05 +0530

    Update favicon and navbar component

---
+++ Frontend/src/Pages/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "../../CSS/HomePageCSS/HomePageNavbar.css";
+export default function () {
+  return (
+    <>
+      <Navbar bg="#0F0F0F" data-bs-theme="dark" sticky="top">
+        {/* changed background color and sticky on top*/}
+        <Container className="NavContainer">
+          <Navbar.Brand href="#home">
+            <img src="../../video (1).png" alt="Icon" />
+            {/* png is from public folder */}
+            <span className="IconText">MyTube</span>
+            {/* <span className="TMText">™</span> */}
+          </Navbar.Brand>
+          <Nav className="me-auto NavbarLinkDiv">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import Navbar from "./Components/Navbar";
 
 export default function HomePage() {
   return (
     <>
-      <Navbar bg="#0F0F0F" data-bs-theme="dark" fixed="top">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Navbar />
     </>
   );
 }
