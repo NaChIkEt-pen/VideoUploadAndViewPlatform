337e911057b7b754210c8f95701f6fcda2444b79
Nachiket Pensalwar <116742714+NaChIkEt-pen@users.noreply.github.com>
2024-02-03 10:19:28 +0530

    Add react-icons package and update Navbar component - Search Bar Added and styled

--- Frontend/src/Pages/Components/Navbar.jsx
+++ Frontend/src/Pages/Components/Navbar.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+// icons
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { AiOutlineVideoCameraAdd } from "react-icons/ai";
+
+// CSS
 import "../../CSS/HomePageCSS/HomePageNavbar.css";
 export default function () {
   return (
@@ -15,12 +26,33 @@ export default function () {
             <span className="IconText">MyTube</span>
             {/* <span className="TMText">™</span> */}
           </Navbar.Brand>
-          <Nav className="me-auto NavbarLinkDiv">
+          {/* <Nav className="me-auto NavbarLinkDiv">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+          </Nav> */}
         </Container>
+        <Form className="searchBarDiv">
+          <Row>
+            <Col xs="auto" className="searchBarTextCol">
+              <Form.Control
+                type="text"
+                placeholder="Search"
+                className=" mr-sm-2 searchBarText"
+              />
+            </Col>
+            <Col xs="auto" className="searchBarBtnCol">
+              <Button type="submit" className="searchBarSeaButton">
+                <FaMagnifyingGlass />
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        <div>
+          {/* <Button type="submit" className="navbarVideoAddButton">
+            <AiOutlineVideoCameraAdd />
+          </Button> */}
+        </div>
       </Navbar>
     </>
   );
