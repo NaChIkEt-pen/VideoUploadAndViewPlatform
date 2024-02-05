4619ef9a57f438d863ff110a5a4deee4287cc69f
Nachiket Pensalwar <116742714+NaChIkEt-pen@users.noreply.github.com>
2024-02-05 09:51:53 +0530

    Add Connected DB - added get req to get user auth.. data

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,46 @@ const app = express()
 const mongoose = require('mongoose');
 const cors = require("cors");
 const { BSON } = require('mongodb');
+require('dotenv').config()
+
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', async (req, res) => {
-    res.send("hello");
+const MongoPass = process.env.MONGO_PASSWORD; //mongodb pass in .env
+mongoose.connect(`mongodb+srv://nachiketpensalwar:${MongoPass}@mymongo.mgg14we.mongodb.net/MyTube`); // connecting the mongo db
+mongoose.pluralize(null); // as mongodb adds a s in the ends that.
+
+const db = mongoose.connection //connection variable
+
+db.on('error', (error) => console.error(error)) //
+db.once('open', () => console.log('Connected to db'))// one time check
+
+const userDataSchema = new mongoose.Schema({ // user data schema 
+  userName: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+
+app.get('/userdata/:id', async (req, res) => { // get api for userdata
+  console.log(req.params.id);
+  try {
+    const userData = mongoose.model("user", userDataSchema);
+    const data = await userData.find()
+    // data.forEach((element,index) => {
+    //   console.log(element)
+    //   console.log(index)
+    // })
+    res.json(data)
+  } catch {
+    res.status(500).json({ message: err.message })
+  }
 })
 
 app.listen(3000, () => {
